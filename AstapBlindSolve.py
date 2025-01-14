332b63e0ffe76c04684b5cad6ae327cc147a1420
Nicolas Castel <nic.castel@gmail.com>
2025-01-14 17:05:24 +0100

    Add a script to blind solve current image with astap cli and update header

---
+++ AstapBlindSolve.py
@@ -0,0 +1,53 @@
+# -*- coding: UTF-8 -*-
+# ==============================================================================
+
+# ------------------------------------------------------------------------------
+# Project: Simple python siril script to run CosmicClarity Sharpen
+#
+# ------------------------------------------------------------------------------
+#    Author:  Nicolas CASTEL <nic.castel (at) gmail.com>
+#
+# This program is provided without any guarantee.
+#
+# The license is  LGPL-v3
+# For details, see GNU General Public License, version 3 or later.
+#                        "https://www.gnu.org/licenses/gpl.html"
+# ------------------------------------------------------------------------------
+
+import sys
+import os
+from siril.connection import SirilInterface
+
+# SET THESE 2 PATHS ! :
+PATH = "/Users/nicolas.castel/Documents/Dev/astap/astap_cli"
+D80 = "/Users/nicolas.castel/Documents/Dev/astap/d80"
+
+print("ASTAP:begin") 
+siril = SirilInterface()
+
+try:
+    siril.connect()
+    
+    FILE = siril.get_image_filename()
+    
+    siril.cmd("save", FILE + "wcs.fit")
+    
+    CMD = PATH + " -f " + FILE + "wcs.fit"
+    CMD += " -D d80 -d " + D80
+    CMD += " -update"
+    
+    print(CMD)
+    
+    print(os.popen(CMD).read())
+    
+    siril.cmd("load",  FILE + "wcs.fit")
+    
+except Exception as e :
+    print("\n**** ERROR *** " +  str(e) + "\n" )    
+
+siril.disconnect()
+del siril
+print("ASTAP:end")
+
+       
+
